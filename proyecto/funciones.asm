cfb5764baf71138c93cacae7c4441f7a1bede0c6
Ulises <ulises5raycas@gmail.com>
2026-06-05 09:19:35 -0600

    Agregando Funcion contar caracteres

--- proyecto/funciones.asm
+++ proyecto/funciones.asm
@@ -13,17 +13,44 @@ validarMovimiento:
     mov rax, r8 ;nuevaFila
     imul rax, rdx ;nuevaFila * columnas
     add rax, r9 ;nuevaFila * columnas + nuevaColumna
-    ;Como cada ountero ocupa 4 bytes, multiplicamos por 4
-    imul rax, 4
     ;Leemos el valor en esa posicion de memoria
-    mov eax,dword [rcx + rax]
-    ;Si el valor es 1, es una pared, retornamos 0 (movimiento no valido)
-    cmp eax, 1
+    mov al,byte [rcx + rax]
+    ;Si el valor es '#', es una pared, retornamos 0 (movimiento no valido)
+    cmp al, 35 ;ASCII de '#'
     je .movimiento_invalido
-    ;Si no es 1 el movimiento es valido, retornamos 1
+    ;Si no es '#', el movimiento es valido, retornamos 1
     mov eax, 1
     ret
 
 .movimiento_invalido:
     mov rax, 0
     ret
+
+    
+    global contarCaracteres
+    ; Función obligatoria 1: contarCaracteres
+    ; Recibe desde C:
+    ; RCX = Dirección inicial del mapa (char*)
+    ; RDX = Número total de celdas (int)
+    ; R8B = Carácter que se desea contar (char de 8 bits)
+    ; Retorna:
+    ; RAX = Cantidad de veces que aparece el carácter
+contarCaracteres:
+    xor rax, rax      ; Inicializamos el contador en 0
+    xor r9, r9        ; Inicializamos el índice en 0
+
+.ciclo_contar:
+    cmp r9, rdx       ; Comparamos el índice actual con el total de celdas
+    jge .fin_contar   ; Si el índice >= totalCeldas, terminamos el ciclo
+    
+    mov r10b, byte [rcx + r9] ; Leemos 1 byte (un carácter) del mapa
+    cmp r10b, r8b     ; Es igual al carácter que estamos buscando?
+    jne .siguiente_contar
+    inc rax           ; Si es igual, incrementamos nuestro contador
+
+.siguiente_contar:
+    inc r9            ; Avanzamos a la siguiente celda
+    jmp .ciclo_contar ; Repetimos el ciclo
+
+.fin_contar:
+    ret               ; Retornamos el total 
